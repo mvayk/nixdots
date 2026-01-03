0aaef727e57fcb5229be4ca1825efed5cbc42395
mvayk <mvayk@mvayk.mvayk>
2026-01-03 19:16:59 +1100

    update

--- common/zshrc.nix
+++ common/zshrc.nix
@@ -10,7 +10,7 @@
             plugins = [
                 "git"
             ];
-            theme = "lamba-glister";
+            theme = "zeta";
             custom = "$HOME/.config/zsh/themes";
             #theme = "half-life";
         };
