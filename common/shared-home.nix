3eba05a6d4f9b7df304209bd3bc4b59133e90d8d
mvayk <45300955+mvayk@users.noreply.github.com>
2026-01-20 05:56:18 +1100

    dude: my grandmother could beat the game if she saved as much as you do

--- common/shared-home.nix
+++ common/shared-home.nix
@@ -1,18 +1,6 @@
 { config, pkgs, machine, theme, ... }:
-let
-    nixDir = ./nix;
-    getNixFiles = dir:
-    if builtins.pathExists dir then
-    let
-        files = builtins.attrNames (builtins.readDir dir);
-        isNixFile = name: builtins.match ".*\.nix$" name != null;
-        nixFiles = builtins.filter isNixFile files;
-    in
-        map (name: "${dir}/${name}") nixFiles
-    else [];
-in
 {
-    imports = getNixFiles nixDir ++ [
+    imports = [
         ../modules/tmux.nix
         ../modules/zshrc.nix
     ];
@@ -21,20 +9,12 @@ in
     home.homeDirectory = "/home/mvayk";
     home.stateVersion = "25.05";
 
-# pray to the overlords that the order of overrides is correct
-    home.file = {
-        ".config" = {
-            source = ./mvayk/.config;
-            recursive = true;
-        };
-    };
-
     programs.git = {
         enable = true;
         settings = {
             user = {
                 name = "mvayk";
-                email = "mvayk@mvayk.mvayk";
+                email = "45300955+mvayk@users.noreply.github.com";
             };
         };
     };
