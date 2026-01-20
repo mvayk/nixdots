120eae7d352f81ffc6ce95709938bfdbb633187f
mvayk <45300955+mvayk@users.noreply.github.com>
2026-01-20 18:50:30 +1100

    update

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -3,66 +3,32 @@
 {
     programs.yazi = {
         enable = true;
-        enableZshIntegration = true;
-
         settings = {
-            manager = {
-                show_hidden = true;
+            yazi = {
                 sort_by = "natural";
-                sort_sensitive = false;
+                sort_sensitive = true;
                 sort_reverse = false;
                 sort_dir_first = true;
-                linemode = "size";
+                linemode = "none";
+                show_hidden = true;
                 show_symlink = true;
             };
 
             preview = {
-                tab_size = 2;
+                image_filter = "lanczos3";
+                image_quality = 90;
+                tab_size = 1;
                 max_width = 600;
                 max_height = 900;
                 cache_dir = "";
-            };
-
-            opener = {
-                edit = [
-                { run = ''$EDITOR "$@"''; block = true; for = "unix"; }
-                ];
-                play = [
-                { run = ''mpv "$@"''; orphan = true; for = "unix"; }
-                ];
-                view_image = [
-                { run = ''feh "$@"''; orphan = true; for = "unix"; }
-                ];
-                view_pdf = [
-                { run = ''zathura "$@"''; orphan = true; for = "unix"; }
-                ];
-                open = [
-                { run = ''xdg-open "$@"''; desc = "Open"; for = "linux"; }
-                { run = ''open "$@"''; desc = "Open"; for = "macos"; }
+                ueberzug_scale = 1;
+                ueberzug_offset = [
+                    0
+                    0
+                    0
+                    0
                 ];
             };
-
-            open = {
-                rules = [
-                { mime = "text/*"; use = "edit"; }
-                { mime = "image/*"; use = "view_image"; }
-                { mime = "video/*"; use = "play"; }
-                { mime = "audio/*"; use = "play"; }
-                { mime = "application/pdf"; use = "view_pdf"; }
-                { mime = "inode/directory"; use = "open"; }
-                ];
-            };
-        };
-
-        keymap = {
-            manager.prepend_keymap = [
-            { on = [ "<C-s>" ]; run = "shell '$SHELL' --block --confirm"; desc = "Open shell"; }
-            { on = [ "l" ]; run = "plugin --sync smart-enter"; desc = "Enter directory or open file"; }
-            { on = [ "g" "h" ]; run = "cd ~"; desc = "Go to home"; }
-            { on = [ "g" "c" ]; run = "cd ~/.config"; desc = "Go to config"; }
-            { on = [ "g" "d" ]; run = "cd ~/Downloads"; desc = "Go to downloads"; }
-            { on = [ "y" ]; run = "yank"; desc = "Yank selected files"; }
-            ];
         };
     };
 }
