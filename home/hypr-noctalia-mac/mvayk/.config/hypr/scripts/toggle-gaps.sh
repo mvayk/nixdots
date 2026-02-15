b4c53ef6d30301859e574afa33552c1005ab61e0
mvayk <45300955+mvayk@users.noreply.github.com>
2026-02-15 20:43:45 +1100

    update

--- home/hypr-noctalia-mac/mvayk/.config/hypr/scripts/toggle-gaps.sh
+++ home/hypr-noctalia-mac/mvayk/.config/hypr/scripts/toggle-gaps.sh
@@ -5,7 +5,7 @@ ROUNDING=$(hyprctl getoption decoration:rounding | grep "int:" | awk '{print $2}
 if [ "$ROUNDING" -eq 0 ]; then
     hyprctl keyword general:gaps_out 8
     hyprctl keyword general:gaps_in 2
-    hyprctl keyword decoration:rounding 20
+    hyprctl keyword decoration:rounding 10
     hyprctl keyword decoration:rounding_power 2
 else
     hyprctl keyword general:gaps_out 0
