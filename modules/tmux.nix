9c51d473d388546dbbdc00109a53d73d337d3a05
mvayk <45300955+mvayk@users.noreply.github.com>
2026-02-17 22:26:35 +1100

    update

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -10,14 +10,26 @@
         escapeTime = 10;
         mouse = true;
         prefix = "C-e";
+        plugins = with pkgs.tmuxPlugins; [
+            resurrect
+            {
+                plugin = continuum;
+                extraConfig = ''
+                    set -g @continuum-restore 'on'
+                    set -g @continuum-save-interval '10'
+                '';
+            }
+            yank
+            tmux-fzf
+        ];
         extraConfig = ''
             # settings
             set-option -g default-shell /run/current-system/sw/bin/zsh
             set-option -g default-terminal "tmux-256color"
             set-option -ga terminal-overrides ",xterm-256color:Tc"
             set -ga terminal-overrides ",*256col*:Tc"
-            set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
-            set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colors
+            set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+            set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
             set-option -g history-limit 5000
             set-option -g mode-keys vi
             set-option -g escape-time 10
@@ -30,6 +42,9 @@
             set-option -g renumber-windows on
             set-option -g focus-events on
 
+            # resurrect
+            set -g @resurrect-capture-pane-contents 'on'
+            set -g @resurrect-strategy-nvim 'session'
 
             # bindings
             unbind C-b
@@ -54,17 +69,47 @@
             unbind %
             bind r source-file ~/.tmux.conf \; display-message "Config reloaded!"
 
-            set-option -g status-style "bg=default"
+            # tmux-fzf
+            bind-key f run-shell -b "${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf/main.sh"
+
+            # sessionx
+            set -g @sessionx-bind 'o'
+            set -g @sessionx-preview-enabled 'true'
+            set -g @sessionx-window-mode 'off'
+            set -g @sessionx-filter-current 'false'
+
+            set-option -g status-style "bg=default,fg=brightblack"
             set-option -g status-position bottom
             set-option -g status-justify left
-            set-option -g status-left-length 100
-            set-option -g status-right-length 100
-            set-option -g status-left "#[bg=default,fg=black]"
-            set-option -g status-right "#[fg=brightblack] #S#[bg=default,fg=black]"
-            set-option -g window-status-format "#[fg=brightblack] #I #W "
+
+            set-option -g status-left-length 80
+            set-option -g status-right-length 80
+
+            # left: session · path · git branch
+            set-option -g status-left "\
+#[fg=brightblack]│\
+#[fg=blue,bold] #S\
+#[fg=brightblack]:   \
+"
+            set-option -g status-right "\
+#[fg=yellow] #(cd #{pane_current_path} && git branch --show-current 2>/dev/null | sed 's/.*/ &/') \
+#[fg=brightblack]\
+#[fg=green]#{=/16/…:pane_current_path}\
+#[fg=brightblack]\
+#[fg=white] %H:%M %d %b \
+#[fg=brightblack]│\
+"
+            # window tabs (centered via status-justify centre)
+            set-option -g window-status-format "#[fg=brightblack]  #I #W  "
             set-option -g window-status-current-format "#[fg=brightblue,bold] #[fg=white,bold]#I #W #[fg=brightblue,bold]"
             set-option -g window-status-separator ""
+
+            # pane borders
+            set-option -g pane-border-style "fg=brightblack"
+            set-option -g pane-active-border-style "fg=blue"
+
+            # message / command bar
+            set-option -g message-style "bg=default,fg=yellow"
         '';
-                #
     };
 }
