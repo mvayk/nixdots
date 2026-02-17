e271eb8cedef321b3da41a255d943efa0f1f10d9
mvayk <45300955+mvayk@users.noreply.github.com>
2026-02-17 21:30:42 +1100

    update

--- home/hypr-noctalia-mac/base.nix
+++ home/hypr-noctalia-mac/base.nix
@@ -679,30 +679,69 @@ wayland.windowManager.hyprland = {
         };
       };
 
-      animations = {
-        enabled = true;
-        bezier = [
-          "linear, 0, 0, 1, 1"
-          "md3_standard, 0.2, 0, 0, 1"
-          "md3_decel, 0.05, 0.7, 0.1, 1"
-          "md3_accel, 0.3, 0, 0.8, 0.15"
-          "overshot, 0.05, 0.9, 0.1, 1.1"
-          "crazyshot, 0.1, 1.5, 0.76, 0.92"
-          "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
-          "fluent_decel, 0.1, 1, 0, 1"
-          "easeInOutCirc, 0.85, 0, 0.15, 1"
-          "easeOutCirc, 0, 0.55, 0.45, 1"
-          "easeOutExpo, 0.16, 1, 0.3, 1"
-        ];
-        animation = [
-          "windows, 1, 3, md3_decel, popin 60%"
-          "border, 1, 1, linear"
-          "borderangle, 1, 80, linear, loop"
-          "fade, 1, 2.5, md3_decel"
-          "workspaces, 1, 3.5, easeOutExpo, slide"
-          "specialWorkspace, 1, 3, md3_decel, slidevert"
-        ];
-      };
+      # animations = {
+      #   enabled = true;
+      #   bezier = [
+      #     "linear, 0, 0, 1, 1"
+      #     "md3_standard, 0.2, 0, 0, 1"
+      #     "md3_decel, 0.05, 0.7, 0.1, 1"
+      #     "md3_accel, 0.3, 0, 0.8, 0.15"
+      #     "overshot, 0.05, 0.9, 0.1, 1.1"
+      #     "crazyshot, 0.1, 1.5, 0.76, 0.92"
+      #     "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+      #     "fluent_decel, 0.1, 1, 0, 1"
+      #     "easeInOutCirc, 0.85, 0, 0.15, 1"
+      #     "easeOutCirc, 0, 0.55, 0.45, 1"
+      #     "easeOutExpo, 0.16, 1, 0.3, 1"
+      #   ];
+      #   animation = [
+      #     "windows, 1, 3, md3_decel, popin 60%"
+      #     "border, 1, 1, linear"
+      #     "borderangle, 1, 80, linear, loop"
+      #     "fade, 1, 2.5, md3_decel"
+      #     "workspaces, 1, 3.5, easeOutExpo, slide"
+      #     "specialWorkspace, 1, 3, md3_decel, slidevert"
+      #   ];
+      # };
+
+
+animations = {
+  enabled = true;
+  bezier = [
+    "wind, 0.05, 0.9, 0.1, 1"
+    "winIn, 0.1, 1.1, 0.1, 1.1"
+    "winOut, 0.3, -0.3, 0, 1"
+    "liner, 1, 1, 1, 1"
+    "md3_standard, 0.2, 0, 0, 1"
+    "md3_decel, 0.05, 0.7, 0.1, 1"
+    "md3_accel, 0.3, 0, 0.8, 0.15"
+    "overshot, 0.05, 0.9, 0.1, 1.1"
+    "crazyshot, 0.1, 1.5, 0.76, 0.92"
+    "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+    "menu_decel, 0.1, 1, 0, 1"
+    "menu_accel, 0.38, 0.04, 1, 0.07"
+    "easeInOutCirc, 0.85, 0, 0.15, 1"
+    "easeOutCirc, 0, 0.55, 0.45, 1"
+    "easeOutExpo, 0.16, 1, 0.3, 1"
+    "softAcDecel, 0.26, 0.26, 0.15, 1"
+    "md2, 0.4, 0, 0.2, 1"
+  ];
+  animation = [
+    "border, 1, 1, liner"
+    "borderangle, 1, 30, liner, loop"
+    "windows, 1, 6, wind, slide"
+    "windowsIn, 1, 6, winIn, slide"
+    "windowsOut, 1, 5, winOut, slide"
+    "windowsMove, 1, 5, wind, slide"
+    "fade, 1, 3, md3_decel"
+    "layersIn, 1, 3, menu_decel, slide"
+    "layersOut, 1, 1.6, menu_accel"
+    "fadeLayersIn, 1, 2, menu_decel"
+    "fadeLayersOut, 1, 4.5, menu_accel"
+    "workspaces, 1, 5, wind, slidevert"
+    "specialWorkspace, 1, 3, md3_decel, slidevert"
+  ];
+};
     };
   };
 
