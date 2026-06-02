e92dd9141e72b75d1c2cc6ad5a1166603fc6893f
mvayk <45300955+mvayk@users.noreply.github.com>
2026-06-03 05:56:08 +1000

    .

--- home/presets/blackmetal/ghostty.nix
+++ home/presets/blackmetal/ghostty.nix
@@ -1,26 +1,26 @@
+{ config, ... }:
 {
-  ...
-}:
-let
-  _settings = {
-    font-family = "VictorMono Nerd Font Mono";
-    font-size = "16";
-    cursor-style-blink = "true";
-    shell-integration = "zsh";
-    keybind = "ctrl+backspace=text:\\x1b\\x7f";
-    window-padding-balance = "true";
-    confirm-close-surface = "false";
-    custom-shader-animation = "always";
-    custom-shader = "cursor_tail.glsl";
-    window-decoration = "false";
-    background-opacity = "0.8";
-    cursor-style = "block";
-    theme = "Black Metal";
-    adjust-cell-height = "+0%";
-    window-padding-x = "5";
-    window-padding-y = "5";
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-family = "VictorMono Nerd Font Mono";
+      font-size = 16;
+      cursor-style-blink = true;
+      shell-integration = "zsh";
+      keybind = "ctrl+backspace=text:\\x1b\\x7f";
+      window-padding-balance = true;
+      confirm-close-surface = false;
+      custom-shader-animation = "always";
+      custom-shader = "${config.xdg.configHome}/ghostty/shaders/cursor_tail.glsl";
+      window-decoration = false;
+      background-opacity = 0.8;
+      cursor-style = "block";
+      theme = "Black Metal";
+      adjust-cell-height = "+0%";
+      window-padding-x = 5;
+      window-padding-y = 5;
+    };
   };
-in
-{
-  xdg.configFile."ghostty/cursor_tail.glsl".source = ../../features/ghostty/cursor_tail.glsl;
+
+  xdg.configFile."ghostty/shaders/cursor_tail.glsl".source = ../../features/ghostty/cursor_tail.glsl;
 }
--- home/presets/noctalia-glass/ghostty.nix
+++ home/presets/noctalia-glass/ghostty.nix
@@ -1,26 +1,27 @@
+{ pkgs, config, ... }:
 {
-  ...
-}:
-let
-  _settings = {
-    font-family = "VictorMono Nerd Font Mono";
-    font-size = "16";
-    cursor-style-blink = "true";
-    shell-integration = "zsh";
-    keybind = "ctrl+backspace=text:\\x1b\\x7f";
-    window-padding-balance = "true";
-    confirm-close-surface = "false";
-    window-decoration = "false";
-    background-opacity = "0.5";
-    theme = "noctalia";
-    cursor-style = "block";
-    adjust-cell-height = "0%";
-    window-padding-x = "10";
-    window-padding-y = "10";
-    custom-shader-animation = "always";
-    custom-shader = "cursor_tail.glsl";
+  programs.ghostty = {
+    enable = true;
+
+    settings = {
+      font-family = "VictorMono Nerd Font Mono";
+      font-size = 16;
+      cursor-style-blink = true;
+      shell-integration = "zsh";
+      keybind = "ctrl+backspace=text:\\x1b\\x7f";
+      window-padding-balance = true;
+      confirm-close-surface = false;
+      window-decoration = false;
+      background-opacity = 0.5;
+      theme = "noctalia";
+      cursor-style = "block";
+      adjust-cell-height = "0%";
+      window-padding-x = 10;
+      window-padding-y = 10;
+      custom-shader-animation = "always";
+      custom-shader = "${config.xdg.configHome}/ghostty/shaders/cursor_tail.glsl";
+    };
   };
-in
-{
-  xdg.configFile."ghostty/cursor_tail.glsl".source = ../../features/ghostty/cursor_tail.glsl;
+
+  xdg.configFile."ghostty/shaders/cursor_tail.glsl".source = ../../features/ghostty/cursor_tail.glsl;
 }
