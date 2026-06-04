c4dd590847e203450969c1fae05f44088249e6b4
mvayk <45300955+mvayk@users.noreply.github.com>
2026-06-04 21:43:19 +1000

    .

--- modules/nixos/de/hyprland.nix
+++ modules/nixos/de/hyprland.nix
@@ -1,9 +1,5 @@
 {
   pkgs,
-  lib,
-  config,
-  quickshell ? null,
-  theme,
   ...
 }:
 {
@@ -21,21 +17,4 @@
 
   environment.etc."xdg/menus/applications.menu".source =
     "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
-
-  programs.dms-shell = {
-    enable = theme == "dank";
-  }
-  // lib.optionalAttrs (theme == "dank") {
-    systemd = {
-      enable = true;
-      restartIfChanged = true;
-    };
-    quickshell.package = quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
-    enableSystemMonitoring = true;
-    enableVPN = true;
-    enableDynamicTheming = true;
-    enableAudioWavelength = true;
-    enableCalendarEvents = true;
-    enableClipboardPaste = true;
-  };
 }
